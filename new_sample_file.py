faf504ef68d3506668539dd303661e3177e652d0
Anushka Agarwal <anushka.agarwal@nutanix.com>
2025-01-28 15:33:30 +0530

    testing

---
+++ new_sample_file.py
@@ -0,0 +1,10 @@
+# Function to multiply two numbers
+def multiply(a, b):
+     return a * b
+
+# Example usage
+num1 = 5
+num2 = 3
+
+result= multiply(num1, num2)
+print(f"The result of {num1} multiplied by {num2} is {result}")
